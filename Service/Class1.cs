0fe61cabad7d0578c0e2355bbf34ddf1cd1f3b09
wilu <wilu@WILU>
2016-11-15 14:05:11 +0100

    ikona, parametry w wątkach

---
+++ Service/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Class1
+    {
+        //Thread t = new Thread(new ParameterizedThreadStart(myMethod));
+        //t.Start (myParameterObject);
+
+        //public Thread StartTheThread(SomeType param1, SomeOtherType param2)
+        //{
+        //    var t = new Thread(() => RealStart(param1, param2));
+        //    t.Start();
+        //    return t;
+        //}
+
+        //private static void RealStart(SomeType param1, SomeOtherType param2)
+        //{
+        //  ...
+        //}
+    }
+}
